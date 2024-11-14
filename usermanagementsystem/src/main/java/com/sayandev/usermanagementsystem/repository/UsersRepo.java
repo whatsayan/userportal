f4c1ba5b339409e6af1aa7cf04b82c98d143defe
whatsayan <tarakbhattacharya375@gmail.com>
2024-11-15 00:22:32 +0530

    first commit

---
+++ usermanagementsystem/src/main/java/com/sayandev/usermanagementsystem/repository/UsersRepo.java
@@ -0,0 +1,11 @@
+package com.sayandev.usermanagementsystem.repository;
+
+import com.sayandev.usermanagementsystem.entity.OurUsers;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsersRepo extends JpaRepository<OurUsers, Integer> {
+
+    Optional<OurUsers> findByEmail(String email);
+}
